8a372e3c509208d25d462eb9a9117e36bf3d209f
Victor Ceron <ceronloezavictor@gmail.com>
2020-10-18 11:14:59 -0500

    Mejora del programa de interaciones

---
+++ Iteraciones.py
@@ -0,0 +1,8 @@
+contador_1 = 0
+contador_2 = 0
+while contador_1 < 5:
+    while contador_2 < 6:
+        print(contador_1, contador_2)
+        contador_2 += 1
+    contador_1 += 1
+    contador_2 = 0
